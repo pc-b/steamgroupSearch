4f6f2ee0cbdeb74d07b19554d726f261453dadc9
pc-b <98758561+pc-b@users.noreply.github.com>
2022-03-21 23:30:00 -0400

    init
    init

---
+++ web-app/src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import JSONDATA from './groups.json'
+import{useState} from 'react'
+function App() 
+{
+  const [searchTerm, setSearchTerm] = useState('');
+  return (
+  <div className="App">
+    <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+    <input type="checkbox" id="public" name="public" value="yes"/>
+    <label for="public">Public</label>
+
+    
+    {JSONDATA.filter((val)=>
+    {
+      if (searchTerm == "")
+      {
+        return val
+      }
+      else if (val.tag.toLowerCase().includes(searchTerm.toLowerCase()))  
+      { /* and check if box ix checked */
+        return val
+      }
+
+    }
+    ).map((val, key)=> 
+    {
+      return <div>
+        {val.name} {val.tag} {val.url} {val.private}
+        </div>
+    })}
+    <header className="App-header"></header>
+
+    
+    
+  </div>
+  );
+}
+
+function checkBox() 
+{
+  var chkBox = document.getElementById('public');
+  if (chkBox.checked)
+  {
+    return true;
+  }
+}
+
+export default App;
