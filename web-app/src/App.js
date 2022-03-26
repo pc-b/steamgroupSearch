5a0a31fa2ec85e2d4ead85d9e754644ee24dc634
pc-b <98758561+pc-b@users.noreply.github.com>
2022-03-26 17:37:14 -0400

    STUFF
    stuff

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import JSONDATA from './py/groups.json'
 import{useState} from 'react'
+import React from 'react';
+
 function App() 
 {
+
+  
   const [searchTerm, setSearchTerm] = useState('');
+
+  
   return (
   <div className="App">
     <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
-    <input type="checkbox" id="public" name="public" value="yes"/>
+    <input  type="checkbox" id="public" name="public" value="yes"/>
     <label for="public">Public</label>
-
     
     {JSONDATA.filter((val)=>
     {
@@ -37,13 +42,6 @@ function App()
   );
 }
 
-function checkBox() 
-{
-  var chkBox = document.getElementById('public');
-  if (chkBox.checked)
-  {
-    return true;
-  }
-}
+
 
 export default App;
