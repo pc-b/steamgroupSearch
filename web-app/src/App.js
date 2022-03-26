a72424c8dc185e4330dfabf3ff822582e627a7c3
pc-b <98758561+pc-b@users.noreply.github.com>
2022-03-26 19:02:20 -0400

    fixx forexan
    fixes

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,34 +4,40 @@ import{useState} from 'react'
 import React from 'react';
 
 function App() 
-{
-
-  
+{  
   const [searchTerm, setSearchTerm] = useState('');
+
+  function handleChange() {
+    if (document.getElementById("public").checked === true) {
+      console.log("checked")
+      return true
+    }
+    else {
+      console.log("unchecked")
+      return false
+    }
+  }
+
 
   
   return (
   <div className="App">
-    <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
-    <input  type="checkbox" id="public" name="public" value="yes"/>
-    <label for="public">Public</label>
+    <input type="text" class="search" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+    <input type="checkbox"  id="public" name="box" onChange={handleChange}/>
+    <label for="public">Public?</label>
+
     
     {JSONDATA.filter((val)=>
     {
-      if (searchTerm == "")
-      {
-        return val
-      }
-      else if (val.GroupTag.toLowerCase().includes(searchTerm.toLowerCase()))  
+      if (val.GroupTag.toLowerCase().substring(0, searchTerm.length).match(searchTerm))  
       { /* and check if box ix checked */
         return val
       }
-
     }
     ).map((val, key)=> 
     {
       return <div>
-        {val.GroupName} {val.GroupTag} {val.GroupURL} {val.IsPrivate}
+        <a href={val.GroupURL}>{val.GroupName}</a> {val.GroupTag} {val.IsPrivate}
         </div>
     })}
     <header className="App-header"></header>
